ab90787bc035e42bca82c7e3ac2c11c475747e58
Abubaker Tagelsir <abubaker.tagelsir@gmail.com>
2019-09-10 17:55:56 +0400

    Aliens Part and collisions effects and allow up to 4  ships per game

--- alien_invasion.py
+++ alien_invasion.py
@@ -5,6 +5,7 @@ from settings import Settings
 from ship import Ship
 import game_functions as gf
 from pygame.sprite import Group
+from game_stats import GameStats
 
 def run_game():
     # initialize game and create screen object and settings
@@ -13,19 +14,26 @@ def run_game():
     screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
 
+    stats = GameStats(ai_settings)
+
     # make a ship
     ship = Ship(ai_settings, screen)
 
     #make groups to store bullets in
     bullets = Group()
+    aliens = Group()
+
+    gf.create_fleet(ai_settings, screen, ship, aliens)
 
     # start the main loop for the game
     while True:
         # watch keyboard and mouse events
         gf.check_events(ai_settings, screen, ship, bullets)
-        ship.update()
-        gf.update_bullets(bullets)
+        if stats.game_active:
+                ship.update()
+                gf.update_bullets(ai_settings, screen, ship, aliens, bullets)
+                gf.update_aliens(ai_settings, stats, screen, ship, aliens, bullets)
         # print(len(bullets))
-        gf.update_screen(ai_settings, screen, ship, bullets)
+        gf.update_screen(ai_settings, screen, ship, aliens, bullets)
 
 run_game()
